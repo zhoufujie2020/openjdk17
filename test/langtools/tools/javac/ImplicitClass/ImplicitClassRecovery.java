e52e361158cd2bad538078acbee7fa178486ab31
Chou20 <71582445+zhoufujie2020@users.noreply.github.com>
2024-01-09 11:24:12 +0800

    Delete test directory

--- test/langtools/tools/javac/ImplicitClass/ImplicitClassRecovery.java
+++
@@ -1,8 +0,0 @@
-/**
- * @test /nodynamiccopyright/
- * @compile/fail/ref=ImplicitClassRecovery.out -XDrawDiagnostics --enable-preview --source ${jdk.version} ImplicitClassRecovery.java
- */
-public void main() {
-    //the following is intentionally missing a semicolon:
-    System.err.println("Hello!")
-}
--- test/micro/org/openjdk/bench/java/lang/foreign/xor/XorOp.java
+++
@@ -1,7 +0,0 @@
-package org.openjdk.bench.java.lang.foreign.xor;
-
-public interface XorOp {
-
-    void xor(byte[] src, int sOff, byte[] dst, int dOff, int len) throws Throwable;
-
-}
